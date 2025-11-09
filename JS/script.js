cf696facab35af5f08385fcc25b8ad04e9d01a65
oranerachel-pixel <orane.rachel@gmail.com>
2025-11-09 16:39:20 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,7 @@
-// script.js
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
+// MENU BURGER MOBILE
+const burger = document.getElementById('burger');
+const menu = document.querySelector('.menu');
 
+burger.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
